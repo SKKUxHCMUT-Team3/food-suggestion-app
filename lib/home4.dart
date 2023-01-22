1fa73b66cc9db104a6e64713b42358176da29152
Sebyeol23 <lixxil2323@gmail.com>
2023-01-22 16:38:44 +0900

    Front 01/22

--- lib/home4.dart
+++ lib/home4.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'outputsplash.dart';
+import 'package:get/get.dart';
 
 class HomePage4 extends StatefulWidget {
   const HomePage4({super.key});
@@ -9,11 +10,24 @@ class HomePage4 extends StatefulWidget {
 }
 
 class _HomePage4State extends State<HomePage4> {
-  TextEditingController CtrlTextFormField = TextEditingController();
+  TextEditingController CtrlTextFormField1 = TextEditingController();
+  TextEditingController CtrlTextFormField2 = TextEditingController();
+  TextEditingController CtrlTextFormField3 = TextEditingController();
+
+  final GlobalKey<NavigatorState> navigatorKey =
+      new GlobalKey<NavigatorState>();
+
+  var i = 1;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    List<TextEditingController> CtrlTextFormField = [
+      CtrlTextFormField1,
+      CtrlTextFormField2,
+      CtrlTextFormField3
+    ];
+
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
@@ -37,48 +51,118 @@ class _HomePage4State extends State<HomePage4> {
                 width: 300,
                 height: 60,
                 child: TextFormField(
-                  controller: CtrlTextFormField,
+                  controller: CtrlTextFormField[0],
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     enabledBorder: const OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(0)),
-                      borderSide: BorderSide(color: Color(0xffFF8A00), width: 6),
+                      borderSide:
+                          BorderSide(color: Color(0xffFF8A00), width: 6),
                     ),
                     focusedBorder: const OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(0)),
-                      borderSide: BorderSide(color: Color(0xffFF8A00), width: 6),
+                      borderSide:
+                          BorderSide(color: Color(0xffFF8A00), width: 6),
                     ),
                     // hintText: "input food",
-                    suffixIcon: Container(
-                      width: 40,
-                      height: 40,
-                      color: const Color(0xffFF8A00),
-                      margin: const EdgeInsets.fromLTRB(0, 10, 10, 10),
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const OutputSplashScreen()),
-                          );
-                        },
-                        icon: Container(
-                            width: double.infinity,
-                            height: double.infinity,
-                            child: Image.asset(
-                              "assets/images/arrow_button.png",
-                              fit: BoxFit.fill,
-                            )),
-                        padding: const EdgeInsets.all(0),
-                      ),
-                    ),
+                    suffixIcon: removeButton[removeButton.length - 1],
                   ),
                   style: const TextStyle(fontSize: 20),
                 ),
               ),
             ),
+            Column(
+              children: newWidgets,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              TextButton(
+                  onPressed: () {
+                    if (i <= 2) {
+                      setState(() {
+                        if (newTexts.isEmpty) {
+                          newTexts.add(TextButton(
+                            onPressed: () {
+                              Get.to(const OutputSplashScreen());
+                            },
+                            child: const Text("Submit"),
+                          ));
+                        }
+                        if (removeButton.length == 1) {
+                          removeButton.add(null);
+                        }
+                        newWidgets.add(const SizedBox(
+                          height: 15,
+                        ));
+                        newWidgets.add(Center(
+                          child: SizedBox(
+                            width: 300,
+                            height: 60,
+                            child: TextFormField(
+                              controller: CtrlTextFormField[i],
+                              textAlign: TextAlign.center,
+                              decoration: const InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(0)),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffFF8A00), width: 6),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(0)),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffFF8A00), width: 6),
+                                ),
+                              ),
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ));
+                        i++;
+                      });
+                    }
+                  },
+                  child: const Text("More")),
+              getnewTexts(),
+            ]),
           ],
         ),
       ),
     );
+  }
+
+  List<Widget> newWidgets = [];
+  List<Widget> newTexts = [];
+  List removeButton = [
+    Container(
+      width: 40,
+      height: 40,
+      color: const Color(0xffFF8A00),
+      margin: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+      child: IconButton(
+        onPressed: () {
+          Get.to(const OutputSplashScreen());
+        },
+        icon: Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: Image.asset(
+              "assets/images/arrow_button.png",
+              fit: BoxFit.fill,
+            )),
+        padding: const EdgeInsets.all(0),
+      ),
+    ),
+  ];
+
+  Widget getnewTexts() {
+    if (newTexts.isNotEmpty) {
+      return newTexts[0];
+    } else {
+      return const SizedBox(
+        width: 0,
+        height: 0,
+      );
+    }
   }
 }
